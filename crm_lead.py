a3790a453dde412c00b52f59845e5985702ef0d8
Ibrahim Ahmed <43549913+IbrahimAhmed20@users.noreply.github.com>
2025-10-21 14:05:29 +0300

    add new category crm lead have two group user & manager , add user_test filed on crm_lead have two type admin&demo

--- crm_lead.py
+++ crm_lead.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields ,api
 
 class CrmLead(models.Model):
     
-    _inherit = 'crm.lead' 
+    _inherit = 'crm.lead'
+    
     next_call_date = fields.Date(string='Next Call Date')
+    user_test = fields.Selection([
+        ('admin', 'Admin'),
+        ('demo', 'Demo')
+    ], string="User Test")
 
+ 
