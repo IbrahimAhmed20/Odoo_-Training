600b9573e2817daf04ce70e551754c5084f4dd58
Ibrahim Ahmed <43549913+IbrahimAhmed20@users.noreply.github.com>
2025-11-03 23:55:54 +0200

    Add files via upload
    add group to previous task on crmlead.py , create new sub menu name print lead report when click on it open new wizard to print crm lead PDF or excel

--- crm_lead.py
+++ crm_lead.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields ,api
+from odoo import models, fields, api
 
 class CrmLead(models.Model):
-    
     _inherit = 'crm.lead'
-    
-    next_call_date = fields.Date(string='Next Call Date')
-    user_test = fields.Selection([
-        ('admin', 'Admin'),
-        ('demo', 'Demo')
-    ], string="User Test")
 
- 
+    # Next Call Date field
+    next_call_date = fields.Date(string='Next Call Date')
+
+    # User Test field with specific selection options
+    user_test = fields.Selection(
+        [('admin', 'Admin'), ('demo', 'Demo')],
+        string="User Test",
+        required=True,
+        groups="crm_custom.crm_lead_group_manager,crm_custom.crm_lead_group_user"
+    )
+    
+    # Action to open the Print Lead Wizard
+    def action_print_leads(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'print.lead.wizard',
+            'view_mode': 'form',
+            'target': 'new',
+        }
